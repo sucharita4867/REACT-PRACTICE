af786df5b8959e4392afde483d52bd157a2985a6
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-17 23:33:59 +0530

    added this task

--- react-core-concepts-main-part2/src/Album.jsx
+++ react-core-concepts-main-part2/src/Album.jsx
@@ -3,6 +3,7 @@ export default function Album({ album }) {
     <div className="card">
       <h3>name:{album.id}</h3>
       <h3>name:{album.title}</h3>
+      <h3>name:{album.id}</h3>
     </div>
   );
 }
