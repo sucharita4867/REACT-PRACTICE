d33ba0677fefe7c8ec2d2fd920c421ce71ad9d3a
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-18 21:26:05 +0530

    start new practice class

---
+++ react-world-on-the-go/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Countries from "./componants/countries/countries";
+
+function App() {
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
---
+++ react-world-on-the-go/src/componants/countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Countries = () => {
+  return (
+    <div>
+      <h2>added is file</h2>
+    </div>
+  );
+};
+
+export default Countries;
