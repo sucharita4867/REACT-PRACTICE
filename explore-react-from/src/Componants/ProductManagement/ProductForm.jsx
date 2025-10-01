8d6da11a14077da3cb0c7326160acc4088be75e1
sucharita4867 <sucharitasardar7@gmail.com>
2025-10-01 14:05:04 +0530

    complete this task prarctice

--- explore-react-from/src/Componants/ProductManagement/ProductForm.jsx
+++ explore-react-from/src/Componants/ProductManagement/ProductForm.jsx
@@ -2,16 +2,23 @@ import React, { useState } from "react";
 
 const ProductForm = ({ handleProduct }) => {
   //   console.log(handleProduct);
+  const [error, setError] = useState("");
   const handleProductSubmit = (e) => {
-    const [error, setError] = useState("");
     e.preventDefault();
     const name = e.target.name.value;
     const price = e.target.price.value;
     const quantity = e.target.quantity.value;
     //     console.log(name, price, quantity);
     if (name.length === 0) {
-      setError("please ");
-    }
+      setError("please Provide a product name");
+      return;
+    } else if (price.length === 0) {
+      setError("Please provide a price");
+      return;
+    } else if (price < 0) {
+      setError("price can not be negative");
+      return;
+    } else setError("");
     const newProduct = {
       name,
       price,
@@ -30,6 +37,9 @@ const ProductForm = ({ handleProduct }) => {
         <br />
         <input type="submit" value="Submit" />
       </form>
+      <p style={{ color: "red" }}>
+        <small>{error}</small>
+      </p>
     </div>
   );
 };
