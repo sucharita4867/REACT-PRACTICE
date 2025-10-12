422564558a0575f9295698cfa2364a4df4409235
sucharita4867 <sucharitasardar7@gmail.com>
2025-10-12 14:38:22 +0530

    complete signin and sign out in this page

--- simple-firebase-auth/src/Components/Login.jsx
+++ simple-firebase-auth/src/Components/Login.jsx
@@ -1,9 +1,50 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "../firebase/Firebase.init";
+const googleProvider = new GoogleAuthProvider();
 const Login = () => {
+  const [user, setUser] = useState(null);
+
+  const handleGoogleBtnClick = () => {
+    // console.log("btn clicked");
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSingOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("sign out btn click");
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h2>Please Login</h2>
+      {/* <button onClick={handleGoogleBtnClick}>Sine In With Google</button>
+      <button onClick={handleSingOut}>Sing Out</button> */}
+      {user ? (
+        <button onClick={handleSingOut}>Sing Out</button>
+      ) : (
+        <button onClick={handleGoogleBtnClick}>Sine In With Google</button>
+      )}
+      {user && (
+        <div>
+          <h2>{user?.displayName}</h2>
+          <h4>Email : {user.email}</h4>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };
--- simple-firebase-auth/src/firebase/Firebase.init.js
+++ simple-firebase-auth/src/firebase/Firebase.init.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +12,11 @@ const firebaseConfig = {
   projectId: "simple-firebase-auth-53e1d",
   storageBucket: "simple-firebase-auth-53e1d.firebasestorage.app",
   messagingSenderId: "925076773361",
-  appId: "1:925076773361:web:d5007197f223bc98fa0c50"
+  appId: "1:925076773361:web:d5007197f223bc98fa0c50",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
