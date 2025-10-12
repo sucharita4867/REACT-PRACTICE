efade617ac1f7c3f38654363d6d0cbe7753897c7
sucharita4867 <sucharitasardar7@gmail.com>
2025-10-12 23:16:38 +0530

    all input fild value show in console

--- email-password-auth/src/Componants/Register/Register.jsx
+++ email-password-auth/src/Componants/Register/Register.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 
 const Register = () => {
+  const handelRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log("register btn clicked", email, password);
+  };
   return (
-    <div>
-      <h2>this is register</h2>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form onSubmit={handelRegister}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  className="input"
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
